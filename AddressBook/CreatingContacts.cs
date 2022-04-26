9cfce2a39aeabb118aa5cf072845b704e517ab7a
vishnu244 <vishnunali244@gmail.com>
2022-04-26 15:44:54 +0530

    Creating Contacts

---
+++ AddressBook/CreatingContacts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class CreatingContacts
+    {
+        List<contacts> People = new List<contacts> ();
+        contacts contact = new contacts();
+
+        public void Addperson()
+        {
+            Console.WriteLine("Enter First Name : ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Email : ");
+            contact.Email = Console.ReadLine();
+            
+            Console.WriteLine("Enter Phone Number : ");
+            contact.PhoneNumber = Console.ReadLine();
+        }
+
+        public void output()
+        {
+            Console.WriteLine("Name of the Person : " + contact.FirstName + contact.LastName);
+            Console.WriteLine("Email ID : " + contact.Email);
+            Console.WriteLine("Mobile Number : " + contact.PhoneNumber);
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
 Console.WriteLine("Welcom to Address Book Program!");
+
+CreatingContacts contact = new CreatingContacts();
+
+
+
+while (true)
+{
+    Console.WriteLine("Enter the number :\n1)creatingcontacts");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            contact.Addperson();
+            contact.output();
+            break;
+    }
+}
